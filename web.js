fb21b20dfd62add5a2bae9f774216ae5423a93d2
Baris <baris@bg.local>
2013-09-04 21:02:28 +0200

    index file added

--- web.js
+++ web.js
@@ -2,11 +2,22 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var indexFile;	
+  fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    indexFile = new Buffer(data).toString();
+  });	
+
+  response.send(indexFile);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
